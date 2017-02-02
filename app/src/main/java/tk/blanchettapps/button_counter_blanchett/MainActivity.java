4e219c92969de12c3995a553c568aa5040b3fcdf
William B <blanchettwa@g.cofc.edu>
2017-02-02 11:59:02 -0500

    Commented code, built an APK of the app, and added missing launcher icons.

--- app/src/main/java/tk/blanchettapps/button_counter_blanchett/MainActivity.java
+++ app/src/main/java/tk/blanchettapps/button_counter_blanchett/MainActivity.java
@@ -15,21 +15,40 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Create variables to reference all the objects in the app: buttons, text fields, and internal
+    // properties, such as integers. They must be stored here, because res is permanently final.
+
+    //These buttons handle the basic counting up and counting down
     private Button increment;
     private Button decrement;
     private Button clear;
+
+    //This is the textbox to display the current number
     private TextView viewfield;
-    private EditText leapset;
+
+    //This is the integer for the current count.
     private Integer count = 0;
+
+
+    //These following variables handle the count leaping functionality.
+
+    //This takes in user input to edit what the leap value is.
+    private EditText leapset;
+
+    //In case the leap value is never used, initialize it with a variable.
     private Integer leapnum = 5;
+
+    //Buttons for when the leap should be utilized.
     private Button leapup;
     private Button leapdown;
 
+    //Variables to store the value of the store slots. Initialized to 0 to prevent app crashing.
     private Integer store1 = 0;
     private Integer store2 = 0;
     private Integer store3 = 0;
     private Integer store4 = 0;
 
+    //Buttons to handle the store/save functionality.
     private Button sbutton1;
     private Button sbutton2;
     private Button sbutton3;
@@ -41,6 +60,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Direct each variable to the object it needs to reference.
         increment = (Button) findViewById(R.id.plusbutton);
         decrement = (Button) findViewById(R.id.minusbutton);
         clear = (Button) findViewById(R.id.clearbutton);
@@ -55,38 +75,42 @@ public class MainActivity extends AppCompatActivity {
         sbutton3 = (Button) findViewById(R.id.Store3);
         sbutton4 = (Button) findViewById(R.id.Store4);
 
+        //Correct the text on the store buttons to properly include the current slot values
         sbutton1.setText(getText(R.string.store1) + store1.toString());
         sbutton2.setText(getText(R.string.store2) + store2.toString());
         sbutton3.setText(getText(R.string.store3) + store3.toString());
         sbutton4.setText(getText(R.string.store4) + store4.toString());
 
-        leapset.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
+        //Take action to change the leap value whenever the user changes the text in the box.
+        leapset.addTextChangedListener(new TextWatcher()
+        {
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
 
             @Override
-            public void afterTextChanged(Editable s) {
-                if (s.length() > 0) {
+            public void afterTextChanged(Editable s)
+            {
+                //Prevent crashing by checking if the user enters a value
+                if (s.length() > 0)
+                {
+                    //If a value is there, use it for leapnum
                     leapnum = Integer.valueOf(s.toString());
                 }
                 else
                 {
+                    //If the text box is empty, reset the leapnum to its default value.
                     leapnum = 5;
                 }
             }
         });
 
-        leapup.setOnClickListener(new View.OnClickListener() {
+        leapup.setOnClickListener (new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //When " Leap +" is clicked, add the leap number to the current value.
                 count += leapnum;
+
+                //Change the text box to reflect the current number's value
                 viewfield.setText(count.toString());
             }
         });
@@ -94,17 +118,21 @@ public class MainActivity extends AppCompatActivity {
         leapdown.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //When " Leap -" is clicked, subtract the leap number from the current value.
                 count -= leapnum;
+
+                //Change the text box to reflect the current number's value
                 viewfield.setText(count.toString());
             }
         });
+
         increment.setOnClickListener(new View.OnClickListener(){
 
             public void onClick(View v){
-                System.out.println("Value originally: " + count.toString());
-
+                //Add one to the current value when + is clicked
                 count += 1;
-                System.out.println("Value Should now be" + count);
+
+                //Reflect the change in the visible text box
                 viewfield.setText(count.toString());
 
             }
@@ -112,32 +140,36 @@ public class MainActivity extends AppCompatActivity {
 
         decrement.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                System.out.println("Value originally: " + count);
-
+                //Subtract one from the current value when - is clicked
                 count -= 1;
-                System.out.println("Value Should now be" + count);
+
+                //Reflect the change in value in the text box
                 viewfield.setText(count.toString());
-
-
             }
         });
 
         clear.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
-                System.out.println("Setting current value " + count + " to Zero.");
 
+                //If this button is clicked, reset the value to 0. Reflect the change.
                 count = 0;
                 viewfield.setText(count.toString());
             }
         });
 
+        //If a slot button is held...
         sbutton1.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
+                //...save it in the correct slot...
                 store1 = count;
+
+                //...update the button text to show that a number is saved...
                 sbutton1.setText(getText(R.string.store1) + store1.toString());
+
+                //...and give the user some notification via toast message.
                 Toast.makeText(getApplicationContext(), "Value stored in slot 1!", Toast.LENGTH_SHORT).show();
-                return false;
+                return false; //Why is this listener a boolean anyway?
             }
         });
 
@@ -171,11 +203,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Should the user tap a store button...
         sbutton1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //...set the current value to the stored number...
                 count = store1;
-                viewfield.setText(count.toString());
+                viewfield.setText(count.toString()); //...reflect the change, then give feedback
                 Toast.makeText(getApplicationContext(), "Value restored from slot 1!", Toast.LENGTH_SHORT).show();
             }
         });
@@ -213,8 +247,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState) {
+    protected void onSaveInstanceState(Bundle outState)
+    {
         super.onSaveInstanceState(outState);
+
+        //Save important numbers, such as the current value, the leap number, and the stored slots
         outState.putInt("countvalue",count);
         outState.putInt("leapy", leapnum);
         outState.putInt("s1", store1);
@@ -225,20 +262,28 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    {
         super.onRestoreInstanceState(savedInstanceState);
+
+        //Return the count to its former place and reflect the change
         count = savedInstanceState.getInt("countvalue");
         viewfield.setText(count.toString());
+
+        // temporarily hold the leap value so tests can be run.
         Integer tempnum = savedInstanceState.getInt("leapy");
 
-        if (tempnum == 5) {
-            leapnum = 5;
-        }else
+        //if the value is the default, then just reset it to the default
+        if (tempnum == 5) {leapnum = 5;}
+
+        else
         {
+            //If its not the default, restore the number, then reflect the current value in the text box
             leapnum = tempnum;
             leapset.setText(leapnum.toString());
         }
 
+        //Restore the store slots to their previous values, then reflect the change in the button labels.
         store1 = savedInstanceState.getInt("s1");
         store2 = savedInstanceState.getInt("s2");
         store3 = savedInstanceState.getInt("s3");
@@ -247,9 +292,6 @@ public class MainActivity extends AppCompatActivity {
         sbutton2.setText(getText(R.string.store2) + store2.toString());
         sbutton3.setText(getText(R.string.store3) + store3.toString());
         sbutton4.setText(getText(R.string.store4) + store4.toString());
-
-
-
 
     }
 }
